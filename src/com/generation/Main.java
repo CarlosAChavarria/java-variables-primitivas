64290afe97a4347f3f65ff33606e572b0c6bdc8e
Carlos Alberto Chavarria Ortiz <carlosalberto.chavarriao@gmail.com>
2021-10-11 21:20:13 -0500

    Initial commit variables primitivas

---
+++ src/com/generation/Main.java
@@ -0,0 +1,55 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args){
+        String nombre = "Carlos";
+        int edad = 23;
+        System.out.println("Hola " + nombre + " tienes: " + edad + " años");
+/*
+//        tipos de variables
+        //byte num1 = 10;
+        //short num2 = 100;
+        int num3 = 10;
+        long num4 = 10;
+
+        float decimal1 = 10.56f;
+        double decimal2 = 20.123456789123456d;
+
+        //System.out.println(num1);
+        System.out.println(decimal2);
+
+//        cambio de tipo de dato
+        byte numByte1 = 10;
+//        conversion tipo byte a short
+        short numShort1 = numByte1;
+
+//        cambio de tipo de dato entero a byte
+        int numInt = 127;
+        byte intAbyte = (byte) numInt;
+        System.out.println(intAbyte);
+
+//        suma de valores
+        byte numByte2 = 120;
+        byte numByte3 = 120;
+//        realizamos la conversion para que no se desborde por el tipo de dato
+        //short suma = (short) (numByte2 + numByte3);
+        //System.out.println(suma);
+
+//        operadores aritmeticos +, -, *, /, %
+//        el signo + tambien funciona para concatenar cadenas de texto y variables
+
+//        operaciones basicas*/
+        /*int num1 = 5;
+        int num2 = 2;
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        float division = (float)num1 / num2;
+
+        System.out.println("La suma de " + num1 + " + " + num2 + " es = " + suma);
+        System.out.println("La resta de " + num1 + " - " + num2 + " es = " + resta);
+        System.out.println("La multiplicacion de " + num1 + " * " + num2 + " es = " + multiplicacion);
+        System.out.println("La division de " + num1 + " / " + num2 + " es = " + division);*/
+    }
+}
